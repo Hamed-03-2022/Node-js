e8155a31e4660e8489734e1ecc718531389eb365
Hamed <hhamid946@yahoo.com>
2024-06-05 07:30:55 +0200

    first runing server

--- Hamed.js
+++ Hamed.js
@@ -1,1 +1,10 @@
-console.log("Hello")
+const http = require('http');
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html'); // Set Content-Type to text/html for HTML content
+    res.end('<h1>Welcome to my first page on Google!</h1>'); // Send HTML content
+});
+
+server.listen(3000, '127.0.0.1', () => {
+    console.log('Server running...');
+});
